402f2c70b23af9e516ca4f23266b18ea29665d75
Fabien Cruz <cruzfabien@gmail.com>
2022-04-08 15:58:41 +0200

    Delete api.js

--- api.js
+++
@@ -1,6 +0,0 @@
-export async function getData(dataPath) {
-  const data = await fetch(dataPath)
-    .then((res) => res.json())
-    .catch((err) => console.error(`Error ${err} while fetch`));
-  return data;
-}
